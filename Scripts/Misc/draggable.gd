f317e13206882fb9d8a51f9087a79d27e336d921
Back <kebabasones@interia.pl>
2026-07-07 13:34:10 +0200

    attacking

--- Scripts/Misc/draggable.gd
+++ Scripts/Misc/draggable.gd
@@ -3,6 +3,7 @@ class_name draggable extends Button
 @export var button: Button
 var dragging = false
 @onready var parent = get_parent()
+signal started_dragging
 signal stopped_dragging
 
 func _ready() -> void:
@@ -14,11 +15,13 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	if(dragging):
+		print("im draggin it")
 		parent.global_position = lerp(parent.global_position, get_global_mouse_position(), 0.35);
 		#parent.global_position = get_global_mouse_position();
 		
 func _on_button_down() -> void:
 	dragging = true
+	started_dragging.emit()
 
 
 func _on_button_up() -> void:
--- Scripts/card.gd
+++ Scripts/card.gd
@@ -2,35 +2,121 @@ class_name Card extends Node2D
 
 var last_hovered_slot: Area2D
 var slot: Area2D
+var target: CardSlot
+var state: STATES
+var move_fraction = 0.0
+
+const attack_duration: float = 1.0
+const movement_smoothing: float = 0.2
+const movement_delay: float = 0.5
+const snapping_distance: float = 3
+
+enum STATES{
+	IDLE,
+	ATTACKING,
+	RETURNING,
+	DRAGGING
+}
+
+
 
 func _ready() -> void:
 	$draggable.connect("stopped_dragging", _on_stopped_dragging)
+	$draggable.connect("started_dragging", _on_started_dragging)
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_1:
+			move_fraction = 0.0
+			print("attack my child")
+			attack($"../Enemy/EnemyPortrait/CardSlot")
+		if event.pressed and event.keycode == KEY_2:
+			move_fraction = 0.0
+			print("attack my child")
+			attack($"../Enemy/EnemyPortrait/CardSlot2")
+		if event.pressed and event.keycode == KEY_3:
+			move_fraction = 0.0
+			print("attack my child")
+			attack($"../Enemy/EnemyPortrait/CardSlot3")
+		if event.pressed and event.keycode == KEY_4:
+			move_fraction = 0.0
+			print("attack my child")
+			attack($"../Enemy/EnemyPortrait/CardSlot4")
+		if event.pressed and event.keycode == KEY_5:
+			move_fraction = 0.0
+			print("attack my child")
+			attack($"../Enemy/EnemyPortrait/CardSlot5")
 
 
-func _process(_delta: float) -> void:
-	pass
+func _process(delta: float) -> void:
+	#print(move_fraction)
+	#print(state)
+	match(state):
+		
+		STATES.ATTACKING:
+			if(target == null):
+				state = STATES.RETURNING
+				return
+			print(global_position.distance_to(target.global_position))
+			global_position = lerp(global_position, target.global_position, movement_smoothing)
+			move_fraction = min(move_fraction+delta, movement_delay)
+			if(move_fraction == movement_delay || global_position.distance_to(target.global_position) < snapping_distance):
+				move_fraction = 0.0
+				state = STATES.RETURNING
+			return
+			
+		STATES.RETURNING:
+			if(slot == null):
+				state = STATES.IDLE
+				return
+			global_position = lerp(global_position, slot.get_parent().global_position, movement_smoothing)
+			move_fraction = min(move_fraction+delta, movement_delay)
+			if(move_fraction == movement_delay || global_position.distance_to(slot.get_parent().global_position) < snapping_distance):
+				global_position = slot.get_parent().global_position
+				move_fraction = 0.0
+				state = STATES.IDLE
+			return
+				
+			
+
+		
+		
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	print("wchodze na " + area.get_parent().name)
-	last_hovered_slot = area
+	if(state == STATES.DRAGGING):
+		last_hovered_slot = area
 
 
 func _on_area_2d_area_exited(_area: Area2D) -> void:
-	print("schodze z " + _area.get_parent().name)
-	last_hovered_slot = null
+	if(state == STATES.DRAGGING):
+		last_hovered_slot = null
 	
 func _on_stopped_dragging() -> void:
+	move_fraction = 0.0
 	if(last_hovered_slot != null):
 		if(slot != null):
 			slot.get_parent().remove_card()
 		last_hovered_slot.get_parent().set_card(self)
 		slot = last_hovered_slot
 		last_hovered_slot = null
+		state = STATES.RETURNING
+		return
 	elif(slot != null):
-		snap_back()
+		state = STATES.RETURNING
+		return
+	state = STATES.IDLE
+	
 	
 		
 func snap_back() -> void:
 	global_position = slot.get_parent().global_position
 	
+func attack(new_target: CardSlot) -> void:
+	move_fraction = 0.0
+	target = new_target
+	state = STATES.ATTACKING
+
+func _on_started_dragging() -> void:
+	move_fraction = 0.0
+	state = STATES.DRAGGING
